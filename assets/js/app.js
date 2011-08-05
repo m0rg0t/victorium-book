7e1592418c54d8963f465be9f23845132a532037
Anton Lenev <m0rg0t.Anton@gmail.com>
2011-08-05 21:51:39 +0400

    Main menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,9 +7,9 @@ run(function () {
         navigator.network.isReachable("google.com", function(status) {
 			var connectivity = (status.internetConnectionStatus || status.code || status);
         	if (connectivity === NetworkStatus.NOT_REACHABLE) {
-        		alert("No internet connection - we won't be able to show you any maps");
+        		//alert("No internet connection - we won't be able to show you any maps");
         	} else {
-        		alert("We can reach Google - get ready for some awesome maps!");
+        		//alert("We can reach Google - get ready for some awesome maps!");
         	}
         });
     })();
